1975d68dde871f2fb2b62e0c0f76f55a40bc54f0
zeineb <zainebjalled@gmail.com>
2021-02-17 02:46:44 +0100

    js part

---
+++ js/animation.js
@@ -0,0 +1,163 @@
+let clothes = [
+    {
+      "id": 1,
+      "category": "jeans",
+      "name": "Ripped jeans",
+      "image": "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/11.jpg",
+      "size": "XL",
+      "color": "blue",
+      "price": 20.99
+    },
+    {
+      "id": 2,
+      "category": "jeans",
+      "name": "Boyfriend jeans",
+      "image": "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/10.jpg",
+      "size": "S",
+      "color": "blue",
+      "price": 24.99
+    },
+    {
+      "id": 3,
+      "category": "shirts",
+      "name": "Ripped sweatshirt",
+      "image": "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/7.jpg",
+      "size": "XL",
+      "color": "white",
+      "price": 29.99
+    },
+    {
+      "id": 4,
+      "category": "jackets",
+      "name": "Denim Jacket",
+      "image": "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",
+      "size": "M",
+      "color": "grey",
+      "price": 40.99
+    },
+    {
+      "id": 5,
+      "category": "shirts",
+      "name": "Longsleeve",
+      "image": "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/8.jpg",
+      "size": "L",
+      "color": "black",
+      "price": 120.99
+    },
+    {
+      "id": 6,
+      "category": "jeans",
+      "name": "Ripped jeans",
+      "image": "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/11.jpg",
+      "size": "XL",
+      "color": "blue",
+      "price": 20.99
+    },
+    {
+      "id": 7,
+      "category": "jeans",
+      "name": "Boyfriend jeans",
+      "image": "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/10.jpg",
+      "size": "S",
+      "color": "blue",
+      "price": 24.99
+    },
+    {
+      "id": 8,
+      "category": "shirts",
+      "name": "Ripped sweatshirt",
+      "image": "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/7.jpg",
+      "size": "XL",
+      "color": "white",
+      "price": 29.99
+    },
+    {
+      "id": 9,
+      "category": "jackets",
+      "name": "Denim Jacket",
+      "image": "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",
+      "size": "M",
+      "color": "grey",
+      "price": 40.99
+    },
+    {
+      "id": 10,
+      "category": "shirts",
+      "name": "Longsleeve",
+      "image": "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/8.jpg",
+      "size": "L",
+      "color": "black",
+      "price": 120.99
+    }
+  ];
+  function jeantArt() {
+    var artc = document.createElement("div");
+    var div1 = document.createElement("div");
+    var div2 = document.createElement("div");
+    artc.setAttribute("class", "artc");
+    div1.setAttribute("class", "artc-img");
+    div2.setAttribute("class", "artc-txt");
+    artc.appendChild(div1);
+    artc.appendChild(div2);
+    var htmlArtc = document.getElementById("prod-div");
+    var butn = document.getElementsByClassName("more")[0];
+    htmlArtc.appendChild(artc)
+  }
+  function artcTxt(x) {
+    var name = document.createElement("div");
+    var colSiz = document.createElement("div");
+    var price = document.createElement("div");
+    var btn = document.createElement("div");
+    name.setAttribute("class", "name");
+    colSiz.setAttribute("class", "col-siz");
+    price.setAttribute("class", "price");
+    btn.setAttribute("class", "artc-btn");
+    var htmlTxt = document.getElementsByClassName("artc-txt")[x];
+    htmlTxt.appendChild(name);
+    htmlTxt.appendChild(colSiz);
+    htmlTxt.appendChild(price);
+    htmlTxt.appendChild(btn);
+  }
+  function artcLast(v) {
+    var siz = document.createElement("h3");
+    var color = document.createElement("div");
+    var btn = document.createElement("button");
+    color.setAttribute("class", "color");
+    siz.setAttribute("class", "siz");
+    var btnNode = document.createTextNode("More details");
+    var htmlColSiz = document.getElementsByClassName("col-siz")[v];
+    var htmlBtn = document.getElementsByClassName("artc-btn")[v];
+    htmlColSiz.appendChild(siz);
+    btn.appendChild(btnNode);
+    htmlColSiz.appendChild(color);
+    htmlBtn.appendChild(btn);
+  }
+  function range(nt, k) {
+    var siz = document.getElementsByClassName("siz")[nt];
+    var color = document.getElementsByClassName("color")[nt];
+    var price = document.getElementsByClassName("price")[nt];
+    var artcImg = document.getElementsByClassName("artc-img")[k];
+    var name1 = document.getElementsByClassName("name")[nt];
+    var nameH = document.createElement("h2");
+    var artcImgI = document.createElement("img");
+    var nameNode = document.createTextNode(clothes[k].name);
+    var colorNode = document.createTextNode(clothes[k].color);
+    color.appendChild(colorNode);
+    nameH.appendChild(nameNode);
+    name1.appendChild(nameH);
+    var sizNode = document.createTextNode(clothes[k].size);
+    siz.appendChild(sizNode);
+    var priceNode = document.createTextNode(clothes[k].price);
+    price.appendChild(priceNode);
+    artcImgI.setAttribute("class", "img-part2");
+    artcImgI.setAttribute("src", clothes[k].image);
+    artcImg.appendChild(artcImgI);
+  }
+  var nt = 0;
+  for (k = 0; k < 10; k++) {
+    jeantArt();
+    artcTxt(nt);
+    artcLast(nt);
+    range(nt, k);
+    nt++;
+  }
